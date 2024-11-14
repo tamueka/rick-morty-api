634c310abff6cbaf3f3930469950372b921ebf56
Samuel Ruiz de la Rosa <samuel.ruiz@babelgroup.com>
2024-11-14 22:25:48 +0100

    modify favicon + imoprt react to delete warnings + order imports

--- rick-morty-api/src/main.tsx
+++ rick-morty-api/src/main.tsx
@@ -1,10 +1,11 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './reset.css'
-import App from './App.tsx'
+import * as React from "react";
+import { StrictMode } from "react";
+import { createRoot } from "react-dom/client";
+import App from "./App.tsx";
+import "./reset.css";
 
-createRoot(document.getElementById('root')!).render(
+createRoot(document.getElementById("root")!).render(
   <StrictMode>
     <App />
-  </StrictMode>,
-)
+  </StrictMode>
+);
